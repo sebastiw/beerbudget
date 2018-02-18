dcd6c9114249f43fdd444dc13ecdcec8f89d1770
Sebastian Weddmark Olsson <visnae@gmail.com>
2018-02-18 20:00:04 +0100

    Add store search and cache

--- beerbudget.py
+++ beerbudget.py
@@ -45,9 +45,9 @@ import xml.etree.ElementTree as ET
 from unittest.mock import MagicMock
 
 __CACHE_TIMEOUT__ = 6*24*3600
-__SYSTEMBOLAGET_SORTIMENT_URI__ = 'https://www.systembolaget.se/api/assortment/products/xml'
-__SYSTEMBOLAGET_BUTIKER_URI__ = 'https://www.systembolaget.se/api/assortment/stores/xml'
-__SYSTEMBOLAGET_MAPPNING_URI__ = 'https://www.systembolaget.se/api/assortment/stock/xml'
+__SYSTEMBOLAGET_ASSORTMENT_URI__ = 'https://www.systembolaget.se/api/assortment/products/xml'
+__SYSTEMBOLAGET_STORES_URI__ = 'https://www.systembolaget.se/api/assortment/stores/xml'
+__SYSTEMBOLAGET_MAPPING_URI__ = 'https://www.systembolaget.se/api/assortment/stock/xml'
 
 class Beer:
     def __init__(self, name, price):
@@ -55,14 +55,24 @@ class Beer:
         self.price = float(price)
 
 
+class Store:
+    def __init__(self, name, store_id):
+        self.name = name
+        self.store_id = store_id
+
+
 class Input:
     """Reading and parsing input."""
 
     def __init__(self):
         self.params = None
-        self.cache = 'cache.xml'
+        self.assortment_cache = 'assortment.xml'
+        self.stores_cache = 'stores.xml'
+        self.store_assortment_cache = 'store_assortment.xml'
         self.beers = []
+        self.store = None
         self.searched_beers = []
+        self.searched_stores = []
 
         self.parser = argparse.ArgumentParser(description=__DOCSTRING__,
                                               formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -72,9 +82,12 @@ class Input:
         self.parser.add_argument('--beer', dest='beers', action='append',
                                  help='Add a beer to the calculations.', nargs='+',
                                  metavar='NAME PRICE', default=[])
-        self.parser.add_argument('--search', dest='search', action='append',
+        self.parser.add_argument('--search', dest='search_beer', action='append',
                                  help='Search for a beer to add from Systembolaget.',
                                  nargs='+', metavar='NAME', default=[])
+        self.parser.add_argument('--systembolag', dest='search_store', action='store',
+                                 help='Search for beers on a specific Systembolaget.',
+                                 nargs='+', metavar='NAME', default="")
 
     def parse_args(self, args=None):
         self.params = self.parser.parse_args(args)
@@ -82,8 +95,8 @@ class Input:
         self.parse_search()
 
     def parse_search(self):
-        for i in range(len(self.params.search)):
-            self.params.search[i] = " ".join(self.params.search[i])
+        for i in range(len(self.params.search_beer)):
+            self.params.search_beer[i] = " ".join(self.params.search_beer[i])
 
     def parse_beers(self):
         for i in range(len(self.params.beers)):
@@ -91,43 +104,53 @@ class Input:
             price = float(self.params.beers[i][-1])
             self.beers.append(Beer(name, price))
 
-    def search(self):
-        if len(self.params.search):
-            self.check_cache()
-            print("SEARCH...")
+    def search_beer(self):
+        if len(self.params.search_beer):
+            self.check_cache(self.assortment_cache, __SYSTEMBOLAGET_ASSORTMENT_URI__)
             self.find_beers()
-            self.choose_multiple_matches()
+            self.choose_multiple_beers()
 
-    def check_cache(self):
-        if (not os.path.isfile(self.cache) or
-            (time.time()-os.path.getmtime(self.cache)) > __CACHE_TIMEOUT__):
-            self.download_systembolaget()
+    def search_store(self):
+        if len(self.params.search_store):
+            self.params.search_store = "".join(self.params.search_store)
+
+            self.check_cache(self.stores_cache, __SYSTEMBOLAGET_STORES_URI__)
+            self.check_cache(self.store_assortment_cache, __SYSTEMBOLAGET_MAPPING_URI__)
+            self.find_store()
+            self.choose_multiple_stores()
+
+    def check_cache(self, cache_file, download_uri):
+        if (not os.path.isfile(cache_file) or
+            (time.time()-os.path.getmtime(cache_file)) > __CACHE_TIMEOUT__):
+            self.download_cache(cache_file, download_uri)
             return 1
         else:
-            print("Cache ok.")
             return 0
 
-    def download_systembolaget(self):
-        print("Downloading cache")
-        r = requests.get(__SYSTEMBOLAGET_SORTIMENT_URI__)
+    def download_cache(self, cache_file, url):
+        r = requests.get(url)
         if r.status_code == 200:
-            self.save_cache(r.text)
+            self.save_cache(cache_file, r.text)
         else:
             print("Error: %s %s" % (r.status, r.text))
             exit(1)
 
-    def save_cache(self, content):
-        with open(self.cache, 'w') as file:
+    def save_cache(self, cache_file, content):
+        with open(cache_file, 'w') as file:
             file.write(content)
 
     def find_beers(self):
         """Parse cache and search for beers"""
-        with open(self.cache, "r") as file:
+        with open(self.assortment_cache, "r") as file:
             tree = ET.parse(file)
             artiklar = tree.getroot().findall('artikel')
+            patterns = []
+            for search in self.params.search_beer:
+                p = re.compile(search, re.IGNORECASE)
+                patterns.append(p)
+
             for artikel in artiklar:
-                for search in self.params.search:
-                    p = re.compile(search, re.IGNORECASE)
+                for p in patterns:
                     if artikel.find('Namn2').text:
                         name = "%s %s" % (artikel.find('Namn').text,
                                           artikel.find('Namn2').text)
@@ -138,9 +161,22 @@ class Input:
                         pris = artikel.find('Prisinklmoms').text
                         self.searched_beers.append(Beer(name, pris))
 
-    def choose_multiple_matches(self):
+    def find_store(self):
+        """parse cache and search for store"""
+        with open(self.assortment_cache, "r") as file:
+            tree = ET.parse(file)
+            butiker = tree.getroot().findall('ButikOmbud')
+            p = re.compile(self.params.search_store, re.IGNORECASE)
+            for butik in butiker:
+                name = "%s %s" % (butik.find('Namn').text,
+                                  butik.find('Address1').text)
+                if p.match(name):
+                    store_id = butik.find('Nr').text
+                    self.searched_stores.append(Store(name, store_id))
+
+    def choose_multiple_beers(self):
         """If there are multiple matches"""
-        for search in self.params.search:
+        for search in self.params.search_beer:
             p = re.compile(search, re.IGNORECASE)
             matches = []
             for beer in self.searched_beers:
@@ -158,6 +194,24 @@ class Input:
             else:
                 self.beers.append(matches[0])
 
+    def choose_multiple_stores(self):
+        """If there are multiple matches"""
+        p = re.compile(self.params.search_store, re.IGNORECASE)
+        matches = []
+        for store in self.searched_stores:
+            if p.match(store.name):
+                matches.append(store)
+            if len(matches) > 1:
+                # Let the user choose one
+                print("Multiple matches! Choose one of")
+                enum_stores = enumerate(matches)
+                for i,store in enum_stores:
+                    print(i, store.name)
+                no = int(input('Choose an index: '))
+                if no >= 0 and no < len(matches):
+                    self.store = matches[no]
+            else:
+                self.store = matches[0]
 
     def find_bags(self):
         bag = []
@@ -193,17 +247,15 @@ class Test:
         assert x.beers[0].price == 29
         assert x.beers[1].name == "svensk kronvodka"
         assert x.beers[1].price == 200
-        assert x.params.search[0] == "omnipollo leon"
+        assert x.params.search_beer[0] == "omnipollo leon"
 
         pass
 
     def download_test(self):
-        global __SYSTEMBOLAGET_SORTIMENT_URI__
-        uri = __SYSTEMBOLAGET_SORTIMENT_URI__
-        __SYSTEMBOLAGET_SORTIMENT_URI__ = 'http://dummyurl'
+        dummy_url = 'http://dummyurl'
         x = Input()
         try:
-            x.download_systembolaget()
+            x.download_cache("mocked.file", dummy_url)
         except requests.exceptions.ConnectionError as e:
             prefix = ("HTTPConnectionPool(host='dummyurl', port=80): Max "
                       "retries exceeded with url: / (Caused by "
@@ -219,26 +271,27 @@ class Test:
 
     def check_cache_test(self):
         x = Input()
-        x.download_systembolaget = MagicMock(return_value=0)
-        x.cache = "__TEST_CACHE_FILE__.test.tmp"
+        x.download_cache = MagicMock(return_value=0)
+        x.assortment_cache = "__TEST_CACHE_FILE__.test.tmp"
 
-        assert not os.path.isfile(x.cache)
-        assert 1 == x.check_cache()
+        assert not os.path.isfile(x.assortment_cache)
+        assert 1 == x.check_cache(x.assortment_cache, "mocked.uri")
         # Create cache file
-        x.save_cache("TEST")
-        assert 0 == x.check_cache()
+        x.save_cache(x.assortment_cache, "TEST")
+        assert 0 == x.check_cache(x.assortment_cache, "mocked.uri")
         # Check mtime
-        os.utime(x.cache, (time.time()-__CACHE_TIMEOUT__, time.time()-__CACHE_TIMEOUT__))
-        assert 1 == x.check_cache()
+        os.utime(x.assortment_cache, (time.time()-__CACHE_TIMEOUT__, time.time()-__CACHE_TIMEOUT__))
+        assert 1 == x.check_cache(x.assortment_cache, "mocked.uri")
         # Cleanup
-        os.remove(x.cache)
+        os.remove(x.assortment_cache)
 
 if __name__=='__main__':
     Test()
 
     x = Input()
     x.parse_args()
-    x.search()
+    x.search_store()
+    x.search_beer()
 
     print("Choosed beers:")
     for beer in x.beers:
